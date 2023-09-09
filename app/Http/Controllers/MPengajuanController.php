b73d5e4f212209be86003cdb71d6d81ae51c4e4b
Deesudi <theysudi@gmail.com>
2023-09-09 16:25:48 +0800

    api mobile

---
+++ app/Http/Controllers/MPengajuanController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PengajuanController extends Controller
+{
+
+	private $msgGagal = "Gagal Menyimpan Data Penjualan";
+
+
+	// public function FunctionName(Type $var = null)
+	// {
+	//   # code...
+	// }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+	return 'WS';
+});
+
+Route::post('/login', [LoginController::class, 'doLogin']);
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
